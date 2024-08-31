cdd375dc9e10a6c30a8ceaa429715137044c6632
Roaa K <ruaakhuldoon.26@gmail.com>
2024-09-01 01:30:16 +0300

    ADD IMAGE AND HEADER FILE

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
- 
+import Header from '../components/Header'
 import '../styles.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      
+      <Header />
     </>
   )
 }
