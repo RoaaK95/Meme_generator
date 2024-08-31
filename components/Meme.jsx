18640246ff9f8a06773abd93674a644839e4d7b2
Roaa K <ruaakhuldoon.26@gmail.com>
2024-09-01 02:41:43 +0300

    ADD MEME HTML ELEMENTS

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -2,6 +2,29 @@ import React from "react";
 
 export default function Meme(){
     return(
-       <></>
+       <main>
+        <div className="form">
+            <input 
+                type="text"
+                placeholder="Top Text"
+                className="form--input"
+            />
+            <input 
+                type="text"
+                placeholder="Bottom Text"
+                className="form--input"
+            />
+            <button 
+               className="form--button">
+               Get a new meme image 🖼
+            </button>
+
+            <div className="meme">
+             <img src="./images/One-Does-Not-Simply-m1zju.jpg" className="meme--img"/>
+             <h2 className="meme--text top">Top text</h2>
+             <h2 className="meme--text bottom">Bottom text</h2>
+            </div>
+        </div> 
+       </main>
     )
 }
