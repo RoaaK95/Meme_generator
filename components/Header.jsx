451103c89a0b4a5afb30e2cb0498ccc17f882c01
Roaa K <ruaakhuldoon.26@gmail.com>
2024-09-01 02:23:41 +0300

    ADD STYLING TO HEADER.JSX

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-
 export default function Header()
 {
     return(
-        <header>
+        <header className="header">
             <img src="./images/troll-face.png" className="header--img"/>
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React Course - Project 3</h4>
