465d67158f346b9d89ceccccaa5fac4d86e24225
Roaa K <ruaakhuldoon.26@gmail.com>
2024-09-01 02:52:30 +0300

    ADD STYLES TO MEME.JSX

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -15,16 +15,18 @@ export default function Meme(){
                 className="form--input"
             />
             <button 
-               className="form--button">
+               className="form--button"
+            >
                Get a new meme image 🖼
             </button>
+        </div>
 
-            <div className="meme">
-             <img src="./images/One-Does-Not-Simply-m1zju.jpg" className="meme--img"/>
+        <div className="meme">
+             <img src="./images/One-Does-Not-Simply-m1zju.jpg" className="meme--image" />
              <h2 className="meme--text top">Top text</h2>
              <h2 className="meme--text bottom">Bottom text</h2>
-            </div>
-        </div> 
+        </div>
+        
        </main>
     )
 }
